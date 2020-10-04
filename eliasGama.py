9ff26831303eae70d10e1fc5fe910b7f90ef08c3
Alan S Mathew <alansmathew@Icloud.com>
2020-10-05 00:11:48 +0530

    elias complet

--- eliasGama.py
+++ eliasGama.py
@@ -15,7 +15,6 @@ def binary(number):
     return int("".join([str(x) for x in ls]))
 
 number=int(input("Enter encoding number: "))
-
 unery=encoading(int(math.log(number,2))+1)
 bin=binary(number)
 
@@ -23,6 +22,12 @@ print("1+log2(%d)="%number,int(math.log(number,2))+1, "unery = %s"%unery)
 print("binary of %d ="%number,bin)
 print("Encorded = ", unery+str(bin)[1:])
 
+
 # decoarding
 
-
+enc=input("Enter the binary : ")
+dec=0
+ls=list(str(int(enc)))[::-1]
+for x in range(0,len(ls)):
+    dec+=(2**(x))*int(ls[x])
+print("Decorded value of %d ="%int(enc),dec)
